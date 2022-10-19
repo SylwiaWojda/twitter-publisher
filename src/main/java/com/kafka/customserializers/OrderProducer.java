5838c1d0dc4c8704c75af415bfd4d89b476a842c
SylwiaWojda <sylwiaaa001@wp.pl>
2022-10-19 16:21:44 +1100

    Create serializer

--- src/main/java/com/kafka/customserializers/OrderProducer.java
+++ src/main/java/com/kafka/customserializers/OrderProducer.java
@@ -1,9 +1,7 @@
-package com.kafka;
+package com.kafka.customserializers;
 
-import com.kafka.customserializers.Order;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 
@@ -14,21 +12,26 @@ public class OrderProducer {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        //props.setProperty("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        props.setProperty("value.serializer", "com.kafka.customserializers.OrderSerializer");
 
 
-        KafkaProducer<String, Integer> producer = new KafkaProducer<String,Integer>(props);
-        ProducerRecord<String,Integer> record = new ProducerRecord<>("OrderTopic", "Mac Book Pro", 10);
+        KafkaProducer<String, Order> producer = new KafkaProducer<String, Order>(props);
+        Order order = new Order();
+        order.setCustomerName("Sylwia");
+        order.setProduct("Iphone");
+        order.setQuantity(1);
+        ProducerRecord<String,Order> record = new ProducerRecord<>("OrderTopic", order.getCustomerName(), order);
 
         try {
-            producer.send(record, new OrderCallback());
+            producer.send(record);
 //            System.out.println(recordMetadata.partition());
 //            System.out.println(recordMetadata.offset());
 //            System.out.println("Message send suc.");
         } catch(Exception e) {
             e.printStackTrace();
         } finally {
-            producer.close();
+            //producer.close();
         }
 
 
