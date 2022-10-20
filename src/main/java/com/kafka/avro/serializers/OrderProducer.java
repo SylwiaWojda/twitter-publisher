7445df8a5c3dd0432c1972c876de778f72dea279
sylwia wojda <sylwiaaa001@wp.pl>
2022-10-20 07:40:04 +0200

    39 create avro schema

--- src/main/java/com/kafka/avro/serializers/OrderProducer.java
+++ src/main/java/com/kafka/avro/serializers/OrderProducer.java
@@ -1,4 +1,32 @@
 package com.kafka.avro.serializers;
 
+import com.kafka.customserializers.Order;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class OrderProducer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("key.serializer", KafkaAvroSerializer.class.getName());
+        props.setProperty("value.serializer", KafkaAvroSerializer.class.getName());
+        props.setProperty("schema.registry.url", "http://localhost:8081");
+
+        KafkaProducer<String, Order> producer = new KafkaProducer<String, Order>(props);
+        Order order = new Order("Sylwia", "Iphone", 3);
+        ProducerRecord<String, Order> record = new ProducerRecord<>("OrderAvroTopic", order.getCustomerName().)
+
+        try {
+            producer.send(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+
+
+    }
 }
