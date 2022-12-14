d8a1bbaec736abd3e415a7c5dbec7e2dd8790d0d
SylwiaWojda <sylwiaaa001@wp.pl>
2022-12-14 12:48:08 +1100

    WIP publisher with spring kafka, sending object User. No tweets. working

--- src/main/java/com/kafka/spring/UserController.java
+++ src/main/java/com/kafka/spring/UserController.java
@@ -1,22 +1,20 @@
 package com.kafka.spring;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/userapi")
+@RequestMapping("/a")
 public class UserController {
 
     @Autowired
     private UserProducerService service;
 
-    @PostMapping("/publishUserData")
-    public void sendUserData(@RequestBody User user) {
+    @GetMapping("/b")
+    public void sendUserData() {
+        User user = new User();
+        user.setAge(123);
+        user.setName("Ania");
         service.sendUserData(user);
     }
 }
